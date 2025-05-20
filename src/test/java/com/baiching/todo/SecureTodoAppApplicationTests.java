98be84ea0a55b9a1b351022ff22631c2f3b66270
baiching <bachmarma@gmail.com>
2025-05-20 23:15:49 +0200

    initial commit for this

---
+++ src/test/java/com/baiching/todo/SecureTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baiching.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
