516af434d0cf0ed4491efe7f4778b68979ccb6e1
baiching <bachmarma@gmail.com>
2025-05-23 22:56:01 +0200

    added UserRepository class

--- src/main/java/com/baiching/todo/repository/UserRepository.java
+++ src/main/java/com/baiching/todo/repository/UserRepository.java
@@ -1,4 +1,25 @@
 package com.baiching.todo.repository;
 
-public class UserRepository {
+import com.baiching.todo.entity.Todo;
+import com.baiching.todo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    // Find todos by completion status + user
+    List<Todo> findByUserAndCompleted(User user, boolean completed);
+
+    // For admin access to all todos (if needed)
+    List<Todo> findAllByOrderByCreatedAtDesc();
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
 }
