f8c4da2ec3cf650cf8c6c4ea2e16ad0d3baf3fc3
baiching <bachmarma@gmail.com>
2025-05-29 13:26:28 +0200

    added controller for user registration and logout

--- src/main/java/com/baiching/todo/controller/AuthController.java
+++ src/main/java/com/baiching/todo/controller/AuthController.java
@@ -1,13 +1,20 @@
 package com.baiching.todo.controller;
 
+import com.baiching.todo.entity.ERole;
+import com.baiching.todo.entity.Role;
+import com.baiching.todo.entity.User;
 import com.baiching.todo.repository.RoleRepository;
 import com.baiching.todo.repository.UserRepository;
 import com.baiching.todo.security.jwt.JwtUtils;
 import com.baiching.todo.security.payload.request.LoginRequest;
+import com.baiching.todo.security.payload.request.SignupRequest;
 import com.baiching.todo.security.payload.response.JwtResponse;
+import com.baiching.todo.security.payload.response.MessageResponse;
 import com.baiching.todo.security.services.UserDetailsImpl;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -19,7 +26,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -56,5 +65,66 @@ public class AuthController {
 
         return ResponseEntity.ok(new JwtResponse(jwt, "", userDetails.getId(), userDetails.getUsername(),
                 userDetails.getEmail(), roles));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
+        if (userRepository.existsByUsername(signupRequest.getUsername())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
+        }
+        if (userRepository.existsByEmail(signupRequest.getEmail())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already taken!"));
+        }
+
+        // Creating new user
+        User user = new User();
+        user.setUsername(signupRequest.getUsername());
+        user.setEmail(signupRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
+
+        Set<String> strRoles = signupRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.User)
+                    .orElseThrow(() -> new RuntimeException("Role not found!"));
+            roles.add(userRole);
+        }
+        else {
+            strRoles.forEach(
+                    role -> {
+                        switch (role) {
+                            case "admin":
+                                Role adminRole = roleRepository.findByName(ERole.Admin)
+                                        .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                                roles.add(adminRole);
+
+                                break;
+                            case "mod":
+                                Role modRole = roleRepository.findByName(ERole.Moderator)
+                                        .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                                roles.add(modRole);
+
+                                break;
+                            default:
+                                Role userRole = roleRepository.findByName(ERole.User)
+                                        .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                                roles.add(userRole);
+                        }
+                    }
+            );
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User created successfully!"));
+    }
+
+    @PostMapping("/signout")
+    public ResponseEntity<?> logoutUser(){
+        ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new MessageResponse("You've been signed out!"));
     }
 }
--- src/main/java/com/baiching/todo/security/payload/response/MessageResponse.java
+++ src/main/java/com/baiching/todo/security/payload/response/MessageResponse.java
@@ -3,6 +3,10 @@ package com.baiching.todo.security.payload.response;
 public class MessageResponse {
     private String message;
 
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
     public String getMessage() {
         return message;
     }
