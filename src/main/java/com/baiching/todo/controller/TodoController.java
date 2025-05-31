4780735c0e45058eeaa2d7bf6c3a8fa109c4fe86
baiching <bachmarma@gmail.com>
2025-05-31 19:55:52 +0200

    implemented create todo api endpoint

--- src/main/java/com/baiching/todo/controller/TodoController.java
+++ src/main/java/com/baiching/todo/controller/TodoController.java
@@ -1,4 +1,35 @@
 package com.baiching.todo.controller;
 
+import com.baiching.todo.entity.Todo;
+import com.baiching.todo.entity.User;
+import com.baiching.todo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/todos")
 public class TodoController {
+    @Autowired
+    private TodoService todoService;
+
+    @PostMapping
+    public ResponseEntity<Todo> createTodo(
+            @RequestBody Todo todo,
+            @AuthenticationPrincipal User user) {
+        Todo createdTodo = todoService.createTodo(todo, user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createdTodo.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(createdTodo);
+    }
 }
