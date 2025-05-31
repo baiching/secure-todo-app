9565f60f26e92745b7dc444f0ee9f55f4fcb17a7
baiching <bachmarma@gmail.com>
2025-05-31 19:23:16 +0200

    added crud service for todo

--- src/main/java/com/baiching/todo/service/TodoService.java
+++ src/main/java/com/baiching/todo/service/TodoService.java
@@ -1,4 +1,33 @@
 package com.baiching.todo.service;
 
+import com.baiching.todo.entity.Todo;
+import com.baiching.todo.entity.User;
+import com.baiching.todo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public Todo createTodo(Todo todo, User user){
+        todo.setUser(user);
+        return todoRepository.save(todo);
+    }
+
+    public Todo updateTodo(Todo todo){
+        return todoRepository.findById(todo.getId())
+                .filter(existingTodo -> existingTodo.getId().equals(todo.getId()))
+                .map(existingTodo -> {
+                    existingTodo.setTitle(todo.getTitle());
+                    existingTodo.setDescription(todo.getDescription());
+                    return todoRepository.save(existingTodo);
+                })
+                .orElseThrow(() -> new RuntimeException("Todo not found!"));
+    }
+
+    public void deleteTodo(Long id) {
+        todoRepository.deleteById(id);
+    }
 }
