2f53fe069fc36e5d3f904804adb23feb0db8f772
baiching <bachmarma@gmail.com>
2025-05-31 19:36:25 +0200

    added a service method to get all todos

--- src/main/java/com/baiching/todo/service/TodoService.java
+++ src/main/java/com/baiching/todo/service/TodoService.java
@@ -6,6 +6,8 @@ import com.baiching.todo.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TodoService {
     @Autowired
@@ -29,5 +31,9 @@ public class TodoService {
 
     public void deleteTodo(Long id) {
         todoRepository.deleteById(id);
+    }
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
     }
 }
